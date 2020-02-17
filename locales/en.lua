f43264119ae4a48f5377b87f22d769de9f677be2
istoking <58592678+istoking@users.noreply.github.com>
2020-02-17 23:20:58 +1300

    Create en.lua

---
+++ locales/en.lua
@@ -0,0 +1,12 @@
+Locales['en'] = {
+  ['burgershot'] = 'Burgershot',
+  ['map_blip'] = 'BurgerShot',
+  ['press_menu'] = 'press ~INPUT_CONTEXT~ to access the Menu.',
+  ['burgershot_item'] = '$%s',
+  ['bought'] = 'you just bought %sx %s for $%s',
+  ['not_enough'] = 'you do not have enough money, you\'re missing $%s!',
+  ['player_cannot_hold'] = 'you do not have enough free space in your inventory!',
+  ['burgershot_confirm'] = 'buy %sx %s for $%s?',
+  ['no'] = 'no',
+  ['yes'] = 'yes',
+}
